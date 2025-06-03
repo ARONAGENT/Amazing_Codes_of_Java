0aed799563097ba7c777209da84f23411ce897e9
Rohan Uke <ukerohan7@gmail.com>
2025-06-03 23:39:02 +0530

    Add Even Vs Odd code amazing

---
+++ src/Amazing/PassVSFail.java
@@ -0,0 +1,9 @@
+package Amazing;
+
+public class PassVSFail {
+    public static void main(String[] args) {
+        int marks=42;
+        String[] result ={"Fail","Pass"};
+        System.out.println(result[marks/35]);
+    }
+}
