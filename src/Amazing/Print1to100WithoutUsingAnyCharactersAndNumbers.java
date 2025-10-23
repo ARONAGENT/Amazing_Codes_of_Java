9af1bc6482d5bef711dfbd5832381696c356ec1d
Rohan Uke <ukerohan7@gmail.com>
2025-10-23 21:11:39 +0530

    Print Numbers Without Using any Digit (0 to 9) and Characters (0 to 255) {4}

--- src/Amazing/Print1to100WithoutUsingAnyCharactersAndNumbers.java
+++ src/Amazing/Print1to100WithoutUsingAnyCharactersAndNumbers.java
@@ -1,4 +1,11 @@
 package Amazing;
 
-public class Print1to100WithoutUsingAnyCharactersandNumbers {
+public class Print1to100WithoutUsingAnyCharactersAndNumbers {
+    public static void main(String[] args) {
+        int one=" ".length();
+        int hundred="          ".length()*"          ".length();
+        for (int i = one; i <=hundred ; i++) {
+            System.out.println(i);
+        }
+    }
 }
